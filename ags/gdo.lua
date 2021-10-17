536ee80bb51c95b2c16a156ca11748dd5dae1634
TheRedCreations <35812887+TheRedCreations@users.noreply.github.com>
2021-10-17 11:30:20 +0200

    Create gdo.lua

---
+++ ags/gdo.lua
@@ -0,0 +1,19 @@
+c = require("component")
+event = require("event")
+os = require("os")
+m = c.modem
+
+m.open(985) --send
+m.open(986) --recive
+
+while true do
+  
+  print("IDC?")
+  local code = io.read()
+  print("Send IDC...")
+  m.broadcast(985, code)
+  
+  modem_message = event.listen('modem_message', function(_, _, _, _, _, message, ...)
+   print(tostring(message)) 
+end),
+end

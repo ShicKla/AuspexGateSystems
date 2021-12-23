6c3afa57753370566acf21928c68ae9ad4e9cb4e
ShicKla <shickla@gmail.com>
2021-12-22 22:57:09 -0700

    Changes to kiosk mode and changed variable and function scopes

--- ags/AuspexGateSystems.lua
+++ ags/AuspexGateSystems.lua
@@ -1,6 +1,6 @@
 --[[
 Created By: Augur ShicKla
-v1.1.6
+v1.1.7
 ]]--
 
 local computer = require("computer")
@@ -14,8 +14,6 @@ local gpu = component.gpu
 local internet = nil
 local HasInternet = component.isAvailable("internet")
 if HasInternet then internet = require("internet") end
-
-AGSKiosk = nil
 
 local args, opts = shell.parse(...)
 
@@ -33,19 +31,82 @@ end
 
 if opts.k then
   computer.beep()
-  AGSKiosk = true
+  _G.agsKioskMode = true
 end
 
-ReleaseVersionsFile = "/ags/releaseVersions.ff"
-ReleaseVersions = nil
-LocalVersions = nil
-DialerFound = false
-UsersWorkingDir = nil
-DisplayChangeLog = false
-LogoDisplayed = false
-SelfFileName = string.sub(debug.getinfo(2, "S").source, 2)
+local ReleaseVersionsFile = "/ags/releaseVersions.ff"
+local ReleaseVersions = nil
+local LocalVersions = nil
+local DialerFound = false
+local UsersWorkingDir = nil
+local DisplayChangeLog = false
+local LogoDisplayed = false
+local SelfFileName = string.sub(debug.getinfo(2, "S").source, 2)
 
-function initialization()
+local function downloadFile(fileName, verbose)
+  if verbose then print("Downloading..."..fileName) end
+  local result = ""
+  local response = internet.request(BranchURL..fileName)
+  local isGood, err = pcall(function()
+    local file, err = io.open(fileName, "w")
+    if file == nil then error(err) end
+    for chunk in response do
+      file:write(chunk)
+    end
+    file:close()
+  end)
+  if not isGood then
+    io.stderr:write("Unable to Download\n")
+    io.stderr:write(err)
+    forceExit(false)
+  end
+end
+
+local function displayLogo()
+  if not filesystem.exists("/ags/AuspexLogo.ff") then
+    if HasInternet then
+      downloadFile("/ags/AuspexLogo.ff")
+    else
+      return
+    end
+  end
+  if gpu.maxResolution() >= 160 then
+    LogoDisplayed = true
+    gpu.fill(1, 1, 160, 30, " ")
+    local file = io.open("/ags/AuspexLogo.ff", "r")
+    local i = 1
+    for line in file:lines() do 
+      gpu.set(5, 0+i, line)
+      i = i+1
+    end
+    file:close()
+  end
+end
+
+local function getReleaseVersions()
+  downloadFile(ReleaseVersionsFile)
+  local file = io.open(ReleaseVersionsFile, "r")
+  ReleaseVersions = serialization.unserialize(file:read("*a"))
+  file:close()
+end
+
+local function forceExit(code)
+  if UsersWorkingDir ~= nil then shell.setWorkingDirectory(UsersWorkingDir) end
+  os.exit(code)
+end
+
+local function isVersionGreater(oldVer, newVer)
+  local old = {}
+  local new = {}
+  old[1], old[2], old[3] = string.match(oldVer, "(.*)%.(.*)%.(.*)")
+  new[1], new[2], new[3] = string.match(newVer, "(.*)%.(.*)%.(.*)")
+  for i=1,3 do
+    if tonumber(new[i]) > tonumber(old[i]) then return true end
+  end
+  return false
+end
+
+local function initialization()
   displayLogo()
   if LogoDisplayed then
     -- term.setViewport(160, 50, 0, 31)
@@ -53,7 +114,7 @@ function initialization()
   end
   local yPos = 1
   for line in BranchMsg:gmatch("[^\r\n]+") do
-    -- gpu.set(term.window.width-27, yPos, line)
+    gpu.set(term.window.width-27, yPos, line)
     yPos = yPos + 1
   end
   if HasInternet then
@@ -95,40 +156,7 @@ function initialization()
   end
 end
 
-function displayLogo()
-  if not filesystem.exists("/ags/AuspexLogo.ff") then
-    if HasInternet then
-      downloadFile("/ags/AuspexLogo.ff")
-    else
-      return
-    end
-  end
-  if gpu.maxResolution() >= 160 then
-    LogoDisplayed = true
-    gpu.fill(1, 1, 160, 30, " ")
-    local file = io.open("/ags/AuspexLogo.ff", "r")
-    local i = 1
-    for line in file:lines() do 
-      gpu.set(5, 0+i, line)
-      i = i+1
-    end
-    file:close()
-  end
-end
-
-function forceExit(code)
-  if UsersWorkingDir ~= nil then shell.setWorkingDirectory(UsersWorkingDir) end
-  os.exit(code)
-end
-
-function getReleaseVersions()
-  downloadFile(ReleaseVersionsFile)
-  local file = io.open(ReleaseVersionsFile, "r")
-  ReleaseVersions = serialization.unserialize(file:read("*a"))
-  file:close()
-end
-
-function readVersionFile()
+local function readVersionFile()
   local file = io.open("/ags/installedVersions.ff", "r")
   if file == nil then
     if HasInternet then
@@ -150,21 +178,25 @@ function readVersionFile()
   if opts.d then yPos = 4 end
   local verString = "Launcher: "..LocalVersions.launcher.ver
   if LocalVersions.launcher.dev then verString = verString.." Dev" end
-  -- gpu.set(term.window.width - unicode.len(verString), yPos, verString)
+  gpu.set(term.window.width - unicode.len(verString), yPos, verString)
   if LocalVersions.dialer ~= nil then
     verString = "Dialer: "..LocalVersions.dialer.ver
     if LocalVersions.dialer.dev then verString = verString.." Dev" end
-    -- gpu.set(term.window.width - unicode.len(verString), yPos+1, verString)
+    gpu.set(term.window.width - unicode.len(verString), yPos+1, verString)
   end
 end
 
-function saveVersionFile()
+local function saveVersionFile()
   local file = io.open("/ags/installedVersions.ff", "w")
   file:write(serialization.serialize(LocalVersions))
   file:close()
 end
 
-function launcherVersionCheck(forceDownload)
+local function downloadManifestedFiles(program)
+  for i,v in ipairs(program.manifest) do downloadFile(v, true) end
+end
+
+local function launcherVersionCheck(forceDownload)
   if isVersionGreater(LocalVersions.launcher.ver, ReleaseVersions.launcher.ver) or forceDownload then
     if not forceDownload then print("Launcher needs to update, please wait.") end
     downloadManifestedFiles(ReleaseVersions.launcher)
@@ -182,7 +214,15 @@ function launcherVersionCheck(forceDownload)
   end
 end
 
-function dialerVersionCheck(forceDownload)
+local function changelogShow()
+  if #ReleaseVersions.dialer.note > 0 then
+    print("Changes:")
+    for i,v in ipairs(ReleaseVersions.dialer.note) do print("  "..v) end
+    print()
+  end
+end
+
+local function dialerVersionCheck(forceDownload)
   if LocalVersions.dialer == nil then
     dialerNewInstall()
   elseif isVersionGreater(LocalVersions.dialer.ver, ReleaseVersions.dialer.ver) or forceDownload then
@@ -214,7 +254,7 @@ function dialerNewInstall()
   saveVersionFile()
 end
 
-function compareVersions()
+local function compareVersions()
   if opts.d then
     if not LocalVersions.launcher.dev then
       io.write([[
@@ -282,62 +322,30 @@ function compareVersions()
   end
 end
 
-function isVersionGreater(oldVer, newVer)
-  local old = {}
-  local new = {}
-  old[1], old[2], old[3] = string.match(oldVer, "(.*)%.(.*)%.(.*)")
-  new[1], new[2], new[3] = string.match(newVer, "(.*)%.(.*)%.(.*)")
-  for i=1,3 do
-    if tonumber(new[i]) > tonumber(old[i]) then return true end
-  end
-  return false
-end
-
-function changelogShow()
-  if #ReleaseVersions.dialer.note > 0 then
-    print("Changes:")
-    for i,v in ipairs(ReleaseVersions.dialer.note) do print("  "..v) end
-    print()
-  end
-end
-
-function downloadManifestedFiles(program)
-  for i,v in ipairs(program.manifest) do downloadFile(v, true) end
-end
-
-function downloadFile(fileName, verbose)
-  if verbose then print("Downloading..."..fileName) end
-  local result = ""
-  local response = internet.request(BranchURL..fileName)
-  local isGood, err = pcall(function()
-    local file, err = io.open(fileName, "w")
-    if file == nil then error(err) end
-    for chunk in response do
-      file:write(chunk)
+if not _G.agsAlreadyRunning then
+  _G.agsAlreadyRunning = true
+  
+  initialization()
+  readVersionFile()
+  if HasInternet then compareVersions() end
+  
+  print("Launching Dialer")
+  if _G.agsKioskMode == true then
+    while _G.agsKioskMode do
+      pcall(function() dofile("/ags/SG_Dialer.lua") end)
+      if _G.agsKioskMode then
+        print("\nRestarting AGS Please Wait")
+        os.sleep(2)
+      end
     end
-    file:close()
-  end)
-  if not isGood then
-    io.stderr:write("Unable to Download\n")
-    io.stderr:write(err)
-    forceExit(false)
+  else
+    dofile("/ags/SG_Dialer.lua")
   end
-end
-
-initialization()
-readVersionFile()
-if HasInternet then compareVersions() end
-
-print("Launching Dialer")
--- if LogoDisplayed then term.setViewport(160,50) end
-if AGSKiosk == true then
-  while true do
-    pcall(function() dofile("/ags/SG_Dialer.lua") end)
-    print("\nRestarting AGS Please Wait")
-    os.sleep(2)
-  end
+  
+  _G.agsAlreadyRunning = false
+  shell.setWorkingDirectory(UsersWorkingDir) -- Returns the user back to their original working directory
 else
-  dofile("/ags/SG_Dialer.lua")
+  io.stderr:write("An instance of AGS was already started. If you believe this to be in error, restart the computer.")
 end
 
-shell.setWorkingDirectory(UsersWorkingDir) -- Returns the user back to their original working directory
+
